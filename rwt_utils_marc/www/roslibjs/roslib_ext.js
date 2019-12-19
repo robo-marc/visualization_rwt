b6532c72e34b7d6ee1dcccdc616c674560e12453
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2019-12-19 16:43:30 +0900

    Library fixes

--- rwt_utils_marc/www/roslibjs/roslib_ext.js
+++ rwt_utils_marc/www/roslibjs/roslib_ext.js
@@ -1,10 +1,10 @@
-Ros.prototype.getSrvList = function (callback, failedCallback) {
-  var rwt_srv = new Service({
+ROSLIB.Ros.prototype.getSrvList = function (callback, failedCallback) {
+  var rwt_srv = new ROSLIB.Service({
     ros: this,
     name: '/srv_list',
     serviceType: 'rwt_srv_marc/SrvList'
   });
-  var request = new ServiceRequest();
+  var request = new ROSLIB.ServiceRequest();
   rwt_srv.callService(request, function (result) {
     callback(result);
   }, function (msg) {
@@ -12,17 +12,16 @@ Ros.prototype.getSrvList = function (callback, failedCallback) {
   });
 };
 
-Ros.prototype.getActionList = function (callback, failedCallback) {
-  var rwt_action = new Service({
+ROSLIB.Ros.prototype.getActionList = function (callback, failedCallback) {
+  var rwt_action = new ROSLIB.Service({
     ros: this,
     name: '/action_list',
     serviceType: 'rwt_action_marc/ActionList'
   });
-  var request = new ServiceRequest();
+  var request = new ROSLIB.ServiceRequest();
   rwt_action.callService(request, function (result) {
     callback(result);
   }, function (msg) {
     failedCallback(msg);
   });
-
 };
