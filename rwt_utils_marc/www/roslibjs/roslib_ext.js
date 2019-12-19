b0e8a123ad4b86c638ce0a4b5f75a0acd8cbc21a
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2019-12-19 10:44:07 +0900

    Library correction

--- rwt_utils_marc/www/roslibjs/roslib_ext.js
+++ rwt_utils_marc/www/roslibjs/roslib_ext.js
@@ -2,7 +2,7 @@ Ros.prototype.getSrvList = function (callback, failedCallback) {
   var rwt_srv = new Service({
     ros: this,
     name: '/srv_list',
-    serviceType: 'rwt_srv/SrvList'
+    serviceType: 'rwt_srv_marc/SrvList'
   });
   var request = new ServiceRequest();
   rwt_srv.callService(request, function (result) {
