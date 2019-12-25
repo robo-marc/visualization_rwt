b6ee65335fedbbdaf4ed143fb28b456d993939ac
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2019-12-25 21:08:52 +0900

    add node

--- rwt_topic_marc/www/js/rwt_topic_marc_main.js
+++ rwt_topic_marc/www/js/rwt_topic_marc_main.js
@@ -1,22 +1,39 @@
 
-$(function() {
+$(function () {
 
   // subscribe topic
   var ros = new ROSLIB.Ros();
-  ros.install_config_button("config-button");
+  ros.install_config_button('config-button');
 
-  ros.on("connection", function() {
-    ros.getTopics(function(topic_info) {
+  ros.on('connection', function () {
+    ros.getTopics(function (topic_info) {
       var topics = topic_info.topics;
       topics.sort();
-      $("#topic-select").append(_.map(topics, function(topic) {
-        return '<option value="' + topic + '">' + topic + "</option>";
-      }).join("\n"));
-      $("#topic-select").change();
+      $('#topic-select').append(_.map(topics, function (topic) {
+        return '<option value=' + topic + '>' + topic + '</option>';
+      }).join('\n'));
+      $('#topic-select').change();
     });
   });
 
-  ros.on("close", function() {
-    $("#topic-select").empty();
+  ros.on('close', function () {
+    $('#topic-select').empty();
   });
+
+  $('#execute-button').on('click', function () {
+    var topic = $('#topic-select').val();
+
+    //TODO: getTopictype 
+    ros.getTopicType(topic, function (topicType) {
+
+      console.log(topicType);
+      var type = topicType;
+
+      //TODO: getTopicInfo(hz,bw)
+      ros.getTopicInfo(topic, type, function (value) {
+        console.log(value);
+      });
+    });
+  });
+
 });
