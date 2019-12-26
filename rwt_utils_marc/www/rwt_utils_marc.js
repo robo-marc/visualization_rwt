5f0a8ed34200566ecb3ece74073b4e925a0c9b80
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2019-12-26 12:47:19 +0900

    mod autoConnect

--- rwt_utils_marc/www/rwt_utils_marc.js
+++ rwt_utils_marc/www/rwt_utils_marc.js
@@ -1,5 +1,4 @@
 ROSLIB.Ros.prototype.autoConnect = function () {
-    var that = this;
     var value = "ws://" + location.hostname + ":8888/";
-    that.connect(value);
+    this.connect(value);
 };
