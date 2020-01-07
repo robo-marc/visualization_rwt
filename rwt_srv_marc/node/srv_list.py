fe3abe7b2c786325acbf7d7ae1027641972d8ddf
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2020-01-07 15:37:22 +0900

    modify action_list srv_list

--- rwt_srv_marc/node/srv_list.py
+++ rwt_srv_marc/node/srv_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import roslib; roslib.load_manifest('rwt_srv_marc')
 import rosmsg
 import rospy
 from rospkg.rospack import RosPack
@@ -14,18 +13,13 @@ def handle_rwt_srv(req):
     for pack in package:
         service,path = pack
         message = rosmsg.list_srvs(service)
-        msgList = []
-        for index, messageItem in enumerate(message):
-            msgList.append(str(messageItem))
         messageList.append(str(message))
     return SrvListResponse(messageList)
 
 def srv_list():
     rospy.init_node('srv_list')
     s = rospy.Service('srv_list', SrvList, handle_rwt_srv)
-    print ("stunby ok.")
     rospy.spin()
 
 if __name__ == "__main__":
-
     srv_list()
