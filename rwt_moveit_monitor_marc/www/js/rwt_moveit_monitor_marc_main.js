5673664572aa5994016c6da2be043a7405184e4e
F-ROSROBO <F-ROSROBO@fsi.co.jp>
2020-01-13 13:53:57 +0900

    modify moveit_monitor design

--- rwt_moveit_monitor_marc/www/js/rwt_moveit_monitor_marc_main.js
+++ rwt_moveit_monitor_marc/www/js/rwt_moveit_monitor_marc_main.js
@@ -1,122 +1,176 @@
 
-var nodeGrid;
-var paramGrid;
-var topicGrid;
-var nodeList = [];
-var paramList = [];
-var topicList = [];
+$(function () {
 
-// subscribe topic
-var ros = new ROSLIB.Ros();
-// ros.install_config_button('config-button');
-ros.autoConnect();
+  // TODO getTopics()
+  var topicGrid;
+  var topicList = [];
 
-// NodeListのグリッド
-var nodeColumns = [
-  { id: 'node', name: 'Node', field: 'node', width: 300, minWidth: 20, maxWidth: 800 },
-  { id: 'status', name: 'Status', field: 'status', width: 300, minWidth: 20, maxWidth: 800 },
-];
+  var robot_description = '/robot_description';
+  var robot_description_semantic = '/robot_description_semantic';
 
-function timer() {
-  ros.getNodes(function (nodesData) {
-    nodeList = [];
-    var that = this;
-    console.log(nodesData);
-    if (nodesData.indexOf('/move_group') !== -1) {
-      nodeList.push({
-        node: '/move_group',
-        status: 'Running',
-      })
-    }
-    else {
-      nodeList.push({
-        node: '/move_group',
-        status: 'Not running',
-      })
-    }
+  // subscribe topic
+  var ros = new ROSLIB.Ros();
+  // get parameters
+  var paramGet1 = new ROSLIB.Param({
+    ros: ros,
+    name: robot_description,
+  });
+  var paramGet2 = new ROSLIB.Param({
+    ros: ros,
+    name: robot_description_semantic,
+  });
 
-    console.log(nodeList);
-    paramGrid = new Slick.Grid("#nodeGrid", nodeList, nodeColumns);
+  // initialize screen
+  function initScreen() {
+    // common
+    ros.autoConnect();
+    // get node
+    setInterval(getNode, 5000);
+    // get parameter
+    setInterval(getParam, 5000);
+    // TODO getTopics();
+    setInterval(getTopics, 1000);
+  }
 
-    setTimeout(function () {
-      that.timer();
-    }, 5000);
-  })
-}
+  // TODO getTopics()
+  function getTopics() {
+  }
 
-// ParameterListのグリッド
-var paramcolumns = [
-  { id: 'param_name', name: 'Param name', field: 'param_name', width: 300, minWidth: 20, maxWidth: 800 },
-  { id: 'found', name: 'Found on Parameter Server?', field: 'found', width: 300, minWidth: 20, maxWidth: 800 },
-];
+  // get node
+  function getNode() {
+    ros.getNodes(function (nodesData) {
+      console.log('---- node Data ---');
+      console.log(nodesData);
+      if (nodesData.includes('/move_group')) {
+        $('.status').text('Running');
+      } else {
+        $('.status').text('Not running');
+      }
+    });
+  }
 
-function timer2() {
-  ros.getParams(function (params) {
-    paramList = [];
-    var that = this;
-    console.log(params);
-    if (params.indexOf('/robot_desctiption' && '/robot_description_semantic') !== -1) {
-      paramList.push({
-        param_name: '/robot_desctiption',
-        found: 'yes',
-      })
-      paramList.push({
-        param_name: '/robot_description_semantic',
-        found: 'yes',
-      })
-    }
-    else if (params.indexOf('/robot_desctiption') !== -1 && (params.indexOf('/robot_description_semantic') == -1)) {
-      paramList.push({
-        param_name: '/robot_desctiption',
-        found: 'yes',
-      })
-      paramList.push({
-        param_name: '/robot_description_semantic',
-        found: 'no',
-      })
-    }
-    else if (params.indexOf('/robot_desctiption') == -1 && (params.indexOf('/robot_description_semantic') !== -1)) {
-      paramList.push({
-        param_name: '/robot_desctiption',
-        found: 'no',
-      })
-      paramList.push({
-        param_name: '/robot_description_semantic',
-        found: 'yes',
-      })
-    }
-    else {
-      paramList.push({
-        param_name: '/robot_desctiption',
-        found: 'no',
-      })
-      paramList.push({
-        param_name: '/robot_description_semantic',
-        found: 'no',
-      })
-    }
+  // get parameter list
+  function getParam() {
+    paramGet1.get(function (value) {
+      if (value) {
+        $('#robot_description').text('Yes');
+      } else {
+        $('#robot_description').text('No');
+      }
+    });
 
-    console.log(paramList);
-    paramGrid = new Slick.Grid("#paramGrid", paramList, paramcolumns);
+    paramGet2.get(function (value) {
+      if (value) {
+        $('#robot_description_semantic').text('Yes');
+      } else {
+        $('#robot_description_semantic').text('No');
+      }
+    });
+  }
 
+  // TODO delete
+  // function timer() {
+  //   ros.getNodes(function (nodesData) {
+  //     nodeList = [];
+  //     var that = this;
+  //     console.log(nodesData);
+  //     if (nodesData.indexOf('/move_group') !== -1) {
+  //       nodeList.push({
+  //         node: '/move_group',
+  //         status: 'Running',
+  //       });
+  //     }
+  //     else {
+  //       nodeList.push({
+  //         node: '/move_group',
+  //         status: 'Not running',
+  //       });
+  //     }
+  //     console.log(nodeList);
+  //     paramGrid = new Slick.Grid("#nodeGrid", nodeList, nodeColumns);
+  //     setTimeout(function () {
+  //       that.timer();
+  //     }, 5000);
+  //   });
+  // }
 
-    setTimeout(function () {
-      that.timer2();
-    }, 5000);
-  })
+  // TODO delete
+  // ParameterListのグリッド
+  // var paramcolumns = [
+  //   { id: 'param_name', name: 'Param name', field: 'param_name', width: 300, minWidth: 20, maxWidth: 800 },
+  //   { id: 'found', name: 'Found on Parameter Server?', field: 'found', width: 300, minWidth: 20, maxWidth: 800 },
+  // ];
 
-}
+  // function timer2() {
+  //   ros.getParams(function (params) {
+  //     paramList = [];
+  //     var that = this;
+  //     console.log(params);
+  //     if (params.indexOf('/robot_desctiption' && '/robot_description_semantic') !== -1) {
+  //       paramList.push({
+  //         param_name: '/robot_desctiption',
+  //         found: 'yes',
+  //       });
+  //       paramList.push({
+  //         param_name: '/robot_description_semantic',
+  //         found: 'yes',
+  //       });
+  //     }
+  //     else if (params.indexOf('/robot_desctiption') !== -1 && (params.indexOf('/robot_description_semantic') == -1)) {
+  //       paramList.push({
+  //         param_name: '/robot_desctiption',
+  //         found: 'yes',
+  //       });
+  //       paramList.push({
+  //         param_name: '/robot_description_semantic',
+  //         found: 'no',
+  //       });
+  //     }
+  //     else if (params.indexOf('/robot_desctiption') == -1 && (params.indexOf('/robot_description_semantic') !== -1)) {
+  //       paramList.push({
+  //         param_name: '/robot_desctiption',
+  //         found: 'no',
+  //       });
+  //       paramList.push({
+  //         param_name: '/robot_description_semantic',
+  //         found: 'yes',
+  //       });
+  //     }
+  //     else {
+  //       paramList.push({
+  //         param_name: '/robot_desctiption',
+  //         found: 'no',
+  //       });
+  //       paramList.push({
+  //         param_name: '/robot_description_semantic',
+  //         found: 'no',
+  //       });
+  //     }
 
-//topicListのグリッド（rwt_topicから、グリッド部分を挿入）
-var topicColumns = [
-  { id: 'topic', name: 'Topic', field: 'topic', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
-  { id: 'type', name: 'Type', field: 'type', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
-  { id: 'bandwidth', name: 'Bandwidth', field: 'bandwidth', width: 100, minWidth: 20, maxWidth: 300, sortable: true },
-  { id: 'hz', name: 'Hz', field: 'hz', width: 100, minWidth: 20, maxWidth: 900, sortable: true },
-  { id: 'value', name: 'Value', field: 'value', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
-];
+  //     console.log(paramList);
+  //     paramGrid = new Slick.Grid('#paramGrid', paramList, paramcolumns);
 
-topicGrid = new Slick.Grid("#topicGrid", topicList, topicColumns);
+  //     setTimeout(function () {
+  //       that.timer2();
+  //     }, 5000);
+  //   });
+  // }
 
-timer();
-timer2();
+  //topicListのグリッド（rwt_topicから、グリッド部分を挿入）
+  // var topicColumns = [
+  //   { id: 'topic', name: 'Topic', field: 'topic', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
+  //   { id: 'type', name: 'Type', field: 'type', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
+  //   { id: 'bandwidth', name: 'Bandwidth', field: 'bandwidth', width: 100, minWidth: 20, maxWidth: 300, sortable: true },
+  //   { id: 'hz', name: 'Hz', field: 'hz', width: 100, minWidth: 20, maxWidth: 900, sortable: true },
+  //   { id: 'value', name: 'Value', field: 'value', width: 200, minWidth: 20, maxWidth: 900, sortable: true },
+  // ];
+
+  // topicGrid = new Slick.Grid('#topicGrid', topicList, topicColumns);
+
+  // timer();
+  // timer2();
+
+  // start screen
+  initScreen();
+
+});
